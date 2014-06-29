39b25ea1f5fd379dcbfddca926401232e9c71702
Tim <manglabs@gmail.com>
2014-06-29 09:35:30 -0700

    Exercise 3

---
+++ 003_intro.rb
@@ -0,0 +1,9 @@
+# Exercise 3
+
+Make a little interactive command-line game, using mostly bits and pieces you've seen from the previous two exercises.
+
+See the sample, and crib from it as much as you want!
+
+## References
+
+* [while loops](http://ruby.learncodethehardway.org/book/ex33.html)
